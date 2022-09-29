e9b6dbe98a19fe28d8f4f1ea4128128b325a0911
JohnathanSilva <johnathan.castro.silva@hotmail.com>
2022-09-29 14:20:43 -0300

    Info screen finished

--- src/screens/Info/index.jsx
+++ src/screens/Info/index.jsx
@@ -9,7 +9,36 @@ export function Info(){
     return(
         <div>
             <Header />
-
+                <main className='container-info'>
+                    <div>
+                        <section className='section-info-banner'>
+                            <div className='text-banner-info'> 
+                                <h1>A GELATERIA</h1>
+                            </div>
+                        </section>
+                        <section className='section-info-text'>
+                            <div className='limit-section'>
+                                <h2>Sobre Nós</h2>
+                                <span>Nós simplesmente amamos sorvete!</span>
+                                <p>
+                                    Somos uma empresa apaixonada pelo que faz. Colocamos amor em cada sorvete produzido. Fazemos o melhor sorvete 
+                                    para os nossos clientes e gostamos de receber elogios. Estamos operando desde 2009 com as melhores matérias-primas 
+                                    para a produção final do sorvete. Vendemos tanto para varejo como para atacado.
+                                    Nossos clientes podem comprar os nossos sorvetes e degustar na nossa loja ou levar para sua residência e aproveitar
+                                    junto com a família. Também vendemos para estabelecimentos e criamos eventos como festa de aniversário, formaturas 
+                                    e eventos empresariais. Para contratar os nossos serviços, basta entrar em contato conosco. Iremos proporcionar 
+                                    o melhor atendimento e os melhores produtos para você fazer a sua festa mais saborosa, com o melhor sorvete da cidade.
+                                </p>
+                            </div>
+                        </section>
+                        <section className='section-info-image'>
+                            <div>
+                                <img src="./assets/sobre-image.jpg" alt="Pessoas tomando sorvete" />
+                                <img src="./assets/sorveteria.jpg" alt="Sorveteria" />
+                            </div>
+                        </section>
+                    </div>
+                </main>
             <Footer />
         </div>
     );
